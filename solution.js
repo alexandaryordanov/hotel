8ec096339af0c3a6f9c3ec8f24c9e6aab90fb0a4
Alexandar Yordanov <alexandaryordanov@abv.bg>
2025-01-20 23:50:07 +0200

    File changed

--- solution.js
+++ solution.js
@@ -1,23 +1,14 @@
-let reservation =
-{
-    startDate: null,
-    endDate: null,
-    guestsCount: 0,
-    roomType: null,
-    name: null,
-    phone: null,
-    email: null
+document.querySelector('#confirm-back-btn').addEventListener('click', (e) => getBackToPersonalData(e));
+
+function getBackToPersonalData(e) {
+    e.preventDefault();
+    changeContent('guest-details-form-content');
 }
 
-function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
-    }
-}
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
+document.querySelector('#confirm-reservation').addEventListener('click', (e) => showThanksPage(e));
 
-function cleanData(e) {
-    changeContent('search-form-content');
+function showThanksPage(e) {
+    e.preventDefault();
+    changeContent('thank-you-content');
 }
